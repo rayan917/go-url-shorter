2afbd7227fa37da30f45d8ddd86b556148ec1e91
rmamache <=>
2024-03-19 14:50:56 +0100

    first

---
+++ go.mod
@@ -0,0 +1,13 @@
+module url-short
+
+go 1.21.6
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/mux v1.8.1
+)
+
+require (
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.32.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+
+	"fmt"
+	"hash/crc32"
+
+	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
+)
+
+var client *redis.Client
+
+func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/{shorturl}", redirect).Methods("GET")
+	r.HandleFunc("/", shorten).Methods("POST")
+	http.ListenAndServe(":8080", r)
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	shorturl := mux.Vars(r)["shorturl"]
+	url, err := client.Get(shorturl).Result()
+	if err == redis.Nil {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, "Error getting URL", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, url, http.StatusSeeOther)
+}
+
+func shorten(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	if url == "" {
+		http.Error(w, "Missing URL", http.StatusBadRequest)
+		return
+	}
+	shorturl := generateShortURL(url)
+	err := client.Set(shorturl, url, 0).Err()
+	if err != nil {
+		http.Error(w, "Error saving URL", http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("http://localhost:8080/" + shorturl))
+}
+
+func generateShortURL(url string) string {
+	// This is a very basic way of generating a short URL. In a real application, you would
+	// want to use a more robust method of generating unique short URLs.
+	return fmt.Sprintf("%x", crc32.ChecksumIEEE([]byte(url)))
+}
