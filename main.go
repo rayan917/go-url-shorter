78e21740c8912f450ab31f99f29d987b55f35c05
Jeremy Weltmann <jeremy.we@hotmail.fr>
2024-03-19 17:25:10 +0100

    ajout fonction stats et count link

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"fmt"
 	"hash/crc32"
@@ -23,6 +24,7 @@ func main() {
 	r.HandleFunc("/{shorturl}", redirect).Methods("GET")
 	r.HandleFunc("/", showAll).Methods("GET")
 	r.HandleFunc("/", shorten).Methods("POST")
+	r.HandleFunc("/{shorturl}/stats", stats).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
 
@@ -32,18 +34,23 @@ func showAll(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error getting keys", http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(keys)
+
+	count := 0
 
 	for _, key := range keys {
-		val, err := client.Get(key).Result()
-		if err != nil {
-			http.Error(w, "Error getting value for key "+key, http.StatusInternalServerError)
-			return
+
+		if !strings.Contains(key, "count") {
+			count++
+			fmt.Fprintf(w, "Total links: %d\n", count)
+			val, err := client.Get(key).Result()
+			if err != nil {
+				http.Error(w, "Error getting value for key "+key, http.StatusInternalServerError)
+				return
+			}
+			fmt.Fprintf(w, "%s: %s\n", key, val)
 		}
-		fmt.Fprintf(w, "%s: %s\n", key, val)
 	}
 }
-
 func redirect(w http.ResponseWriter, r *http.Request) {
 	shorturl := mux.Vars(r)["shorturl"]
 	url, err := client.Get(shorturl).Result()
@@ -54,6 +61,7 @@ func redirect(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error getting URL", http.StatusInternalServerError)
 		return
 	}
+	client.Incr("count:" + shorturl)
 	http.Redirect(w, r, url, http.StatusSeeOther)
 }
 
@@ -69,11 +77,22 @@ func shorten(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error saving URL", http.StatusInternalServerError)
 		return
 	}
+	client.Set("count:"+shorturl, 0, 0)
 	w.Write([]byte("http://localhost:8080/" + shorturl))
 }
 
+func stats(w http.ResponseWriter, r *http.Request) {
+	shorturl := mux.Vars(r)["shorturl"]
+	count, err := client.Get("count:" + shorturl).Result()
+
+	if err != nil {
+		http.Error(w, "Error getting count", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Count: " + count))
+}
+
 func generateShortURL(url string) string {
-	// This is a very basic way of generating a short URL. In a real application, you would
-	// want to use a more robust method of generating unique short URLs.
 	return fmt.Sprintf("%x", crc32.ChecksumIEEE([]byte(url)))
 }
